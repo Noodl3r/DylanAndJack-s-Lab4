2094cd6fc5d20a68220997dedd6a756e0e9b38ff
Dylan Sheehan <80593260+Noodl3r@users.noreply.github.com>
2026-03-04 17:03:01 -0500

    Fix mux order in full adder implementation

--- full_Adder_64CS.v
+++ full_Adder_64CS.v
@@ -1,3 +1,4 @@
+`timescale 1ns / 1ns
 module full_Adder_64CS(
     output c_out,
     output [63:0]s,
@@ -10,14 +11,14 @@ module full_Adder_64CS(
     wire c;
     full_Adder_32 FA32_1(c, s[31:0], a[31:0], b[31:0], c_in);
 
-    // second half
+    // Second half
     wire c1, c2;
     wire [31:0] s1,s2;
     full_Adder_32 FA32_2(c1, s1[31:0], a[63:32], b[63:32], 0);
     full_Adder_32 FA32_3(c2, s2[31:0], a[63:32], b[63:32], 1);
 
-    mux_32  MUX_32(s[63:32], s1[31:0], s2[31:0], c);
-    mux_1   MUX_1 (c_out,    c1,       c2,       c);
+    mux_32  MUX_32(s[63:32], s2[31:0], s1[31:0], c);
+    mux_1   MUX_1 (c_out,    c2,       c1,       c);
 
 
 endmodule
